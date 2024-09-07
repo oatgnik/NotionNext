f7352abdf08ee1a7e466e3339ab42e7386e29810
oatgnik <oatgink@qq.com>
2024-09-08 00:36:07 +0800

    修改heo主题

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.oatgnik.top' },
+    // { title: '访问文档中心获取更多帮助', url: 'https://www.oatgnik.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,16 +18,16 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '感受科技',
+  HEO_HERO_TITLE_2: '与文化共鸣',
+  HEO_HERO_TITLE_3: '体验 求实 思索 求真',
+  HEO_HERO_TITLE_4: 'New Zone', 
+  HEO_HERO_TITLE_5: '来者是客，多多交流分享', // 标语
+  HEO_HERO_TITLE_LINK: 'https://www.oatgnik.top', // 点击右侧图片的网页链接
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_2: { title: '闲人碎语', url: '/tag/闲人碎语' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
